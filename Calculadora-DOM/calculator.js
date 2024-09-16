780c1cf8acda8154d914698eb979fa597fc0bdda
Miguel Petit <miguel09112004@gmail.com>
2024-09-16 15:12:24 -0400

    Support to switch to lixun

---
+++ Calculadora-DOM/calculator.js
@@ -0,0 +1,267 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const elements = document.getElementById("elements");
+// elements.addEventListener('input', ()=>{
+//     elements.innerHTML = elements.innerHTML.substring(0, 3);
+// })
+
+// let buttonKeyNumber1 = document.getElementById("button-key-number-1");
+// let initialNumber = "0"; 
+// let limitNumbers = 0;
+// buttonKeyNumber1.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){    
+//             let numberReplace = initialNumber.replace("0", "1");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "1";
+//             limitNumbers++;
+//         }
+    
+//     }
+   
+// })
+
+// let buttonKeyNumber2 = document.getElementById("button-key-number-2");
+
+// buttonKeyNumber2.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "2");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "2";
+//             limitNumbers++;
+//         }
+    
+//     }
+
+// })
+
+// let buttonKeyNumber3 = document.getElementById("button-key-number-3");
+
+// buttonKeyNumber3.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "3");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "3";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// let buttonKeyNumber4 = document.getElementById("button-key-number-4");
+
+// buttonKeyNumber4.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "4");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "4";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// let buttonKeyNumber5 = document.getElementById("button-key-number-5");
+
+// buttonKeyNumber5.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "5");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "5";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// let buttonKeyNumber6 = document.getElementById("button-key-number-6");
+
+// buttonKeyNumber6.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "6");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "6";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// let buttonKeyNumber7 = document.getElementById("button-key-number-7");
+
+// buttonKeyNumber7.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "7");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "7";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// let buttonKeyNumber8 = document.getElementById("button-key-number-8");
+
+// buttonKeyNumber8.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "8");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "8";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// let buttonKeyNumber9 = document.getElementById("button-key-number-9");
+
+// buttonKeyNumber9.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "9");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "9";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// let buttonKeyNumber0 = document.getElementById("button-key-number-0");
+
+// buttonKeyNumber0.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "0");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "0";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// const buttonKeyOperatorAdd = document.getElementById("button-key-operator-+");
+
+// buttonKeyOperatorAdd.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "+");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "+";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// const buttonKeyOperatorSubstract = document.getElementById("button-key-operator-substract");
+
+// buttonKeyOperatorSubstract.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "_");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "_";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// const buttonKeyOperatorMultiply = document.getElementById("button-key-operator-x");
+
+// buttonKeyOperatorMultiply.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "*");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "*";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// const buttonKeyOperatorSplit = document.getElementById("button-key-operator-/");
+
+// buttonKeyOperatorSplit.addEventListener('click', ()=>{
+//     if(limitNumbers < 17){
+//         if(elements.innerHTML == initialNumber){
+//             console.log("if")
+//             let numberReplace = initialNumber.replace("0", "/");
+//             elements.innerHTML = numberReplace;
+//             limitNumbers++;
+//         } else{
+//             elements.innerHTML = elements.innerHTML + "/";
+//             limitNumbers++;
+//         }
+    
+//     }
+// })
+
+// const btnEqual = document.getElementById("button-key-operator-=");
+
+// const btnClean = document.getElementById("button-key-c");   
+
+// btnClean.addEventListener("click", ()=>{
+//     elements.innerHTML = "0"
+// })
+
+
